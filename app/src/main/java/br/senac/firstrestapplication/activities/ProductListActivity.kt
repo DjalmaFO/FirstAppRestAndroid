d0d3884e060fde7719140425149e4f5d07277a0f
DjalmaFO <53957337+DjalmaFO@users.noreply.github.com>
2020-10-22 18:46:06 -0300

    Inserido Package Activities e Atividade para listar produtos

---
+++ app/src/main/java/br/senac/firstrestapplication/activities/ProductListActivity.kt
@@ -0,0 +1,12 @@
+package br.senac.firstrestapplication.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.senac.firstrestapplication.R
+
+class ProductListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_list)
+    }
+}
