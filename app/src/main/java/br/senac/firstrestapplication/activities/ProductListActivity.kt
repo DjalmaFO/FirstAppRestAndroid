f81a1d74b300bab90fa4d4c15378d6c2c6214090
DjalmaFO <53957337+DjalmaFO@users.noreply.github.com>
2020-10-22 19:11:23 -0300

    Inserido função para carregar os produtos na view

--- app/src/main/java/br/senac/firstrestapplication/activities/ProductListActivity.kt
+++ app/src/main/java/br/senac/firstrestapplication/activities/ProductListActivity.kt
@@ -3,10 +3,25 @@ package br.senac.firstrestapplication.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.senac.firstrestapplication.R
+import br.senac.firstrestapplication.model.Product
+import kotlinx.android.synthetic.main.activity_product_list.*
+import kotlinx.android.synthetic.main.product_card_item.view.*
+import java.text.NumberFormat
 
 class ProductListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_list)
+    }
+
+    fun reloadList(products: List<Product>){
+        val formater = NumberFormat.getCurrencyInstance()
+
+        for (product in products){
+            val card = layoutInflater.inflate(R.layout.product_card_item, contProductsList, false)
+            card.tvNomeProductList.text = product.nomeProduto
+            card.tvPrecoProductList.text = formater.format(product.precoProduto)
+            contProductsList.addView(card)
+        }
     }
 }
